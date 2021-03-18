3f67118e35e6f08a730cb11d32e44a9bc136d34c
Yuyuan Chen <yuyuanch@usc.edu>
2021-03-18 23:24:14 +0800

    Update SettingFragment.java

--- CovidMapTeam3/app/src/main/java/com/example/myapplication/ui/setting/SettingFragment.java
+++ CovidMapTeam3/app/src/main/java/com/example/myapplication/ui/setting/SettingFragment.java
@@ -4,18 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterViewAnimator;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.R;
-import com.example.myapplication.ui.home.HomeViewModel;
 
-public class SettingFragment extends Fragment {
+public class SettingFragment extends Fragment  implements AdapterView.OnItemSelectedListener {
 
     private SettingViewModel settingViewModel;
 
@@ -31,6 +36,24 @@ public class SettingFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        Spinner spinner = root.findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(), R.array.cities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
         return root;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
