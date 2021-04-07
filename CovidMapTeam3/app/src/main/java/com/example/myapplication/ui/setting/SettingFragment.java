3f11f9921671857c35cf638c5b67aead1df0e8d3
Yijia Chen <yijiac@usc.edu>
2021-04-07 12:27:43 -0700

    push notification

--- CovidMapTeam3/app/src/main/java/com/example/myapplication/ui/setting/SettingFragment.java
+++ CovidMapTeam3/app/src/main/java/com/example/myapplication/ui/setting/SettingFragment.java
@@ -1,7 +1,13 @@
 package com.example.myapplication.ui.setting;
 
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,8 +23,11 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -35,6 +44,7 @@ public class SettingFragment extends Fragment implements AdapterView.OnItemSelec
     private SettingViewModel settingViewModel;
     private SharedViewModel sharedViewModel;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         settingViewModel =
@@ -63,6 +73,7 @@ public class SettingFragment extends Fragment implements AdapterView.OnItemSelec
             startActivity(intent);
         });
 
+        // dark mode button
         Button btnToggleDark;
         btnToggleDark
                 = root.findViewById(R.id.dark_btn);
@@ -92,6 +103,7 @@ public class SettingFragment extends Fragment implements AdapterView.OnItemSelec
                     }
                 });
 
+        // clear data button
         root.findViewById(R.id.clear_btn).setOnClickListener(v -> {
             AlertDialog.Builder alertDialogBuilder=new AlertDialog.Builder(getContext());
             alertDialogBuilder.setTitle("Clear All Data");
@@ -99,6 +111,35 @@ public class SettingFragment extends Fragment implements AdapterView.OnItemSelec
             AlertDialog alertDialog = alertDialogBuilder.create();
             alertDialog.show(); // Show Dialog
         });
+
+        // send a notification button
+
+        Button sendNotification;
+        sendNotification = root.findViewById(R.id.notification_btn);
+        sendNotification.setOnClickListener(
+                view -> {
+                    CharSequence name = getString(R.string.common_google_play_services_notification_channel_name);
+                    String description = getString(R.string.common_google_play_services_notification_channel_name);
+                    int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                    NotificationChannel channel = new NotificationChannel("my_channel_1", name, importance);
+                    channel.setDescription(description);
+
+                    PendingIntent pendingIntent = PendingIntent.getActivity(getActivity(), 0, new Intent(), 0);
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(), "my_channel_1")
+                            .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                            .setContentTitle("COVID-19 NOTIFICATION")
+                            .setContentText("This is a test notification!")
+                            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                            .setContentIntent(pendingIntent)
+                            .setAutoCancel(true);
+
+                    NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getActivity());
+
+                    // notificationId is a unique int for each notification that you must define
+                    notificationManager.notify(1, builder.build());
+                }
+        );
 
         return root;
     }
