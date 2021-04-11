0fe457b9857e2591cc9b03e4d9c3b7c81a984cb3
Zhian Li <zhianli@usc.edu>
2021-04-11 14:24:40 +0800

    Hide Severity Scale

--- CovidMapTeam3/app/src/main/java/com/example/myapplication/ui/test/TestFragment.java
+++ CovidMapTeam3/app/src/main/java/com/example/myapplication/ui/test/TestFragment.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
@@ -94,6 +95,8 @@ public class TestFragment extends Fragment implements OnMapReadyCallback {
             captureScreen();
         });
 
+        ImageView severityScale = root.findViewById(R.id.imageView2);
+        severityScale.setVisibility(View.INVISIBLE);
         return root;
     }
 
